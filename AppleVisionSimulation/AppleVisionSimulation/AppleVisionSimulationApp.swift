922324bc37ebec5b7f244ee78cd09fa029b6a819
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-04-24 13:59:31 +0300

    Add files via upload

---
+++ AppleVisionSimulation/AppleVisionSimulation/AppleVisionSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleVisionSimulationApp.swift
+//  AppleVisionSimulation
+//
+//  Created by Berkan Aydın on 23.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleVisionSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
